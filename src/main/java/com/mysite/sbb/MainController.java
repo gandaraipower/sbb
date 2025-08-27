0c7c3cebe26df127a1387400112d9cc71277e3d4
gandaraipower <jjangjongsoo@naver.com>
2025-08-27 11:09:13 +0900

    4강

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,13 +2,15 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/")
-    public void index(){
-        System.out.println("index() 메서드 호출됨!");
+    @GetMapping("/index")
+    @ResponseBody   //URL 요청에 대한 응답으로 문자열을 리턴하라
+    public String index(){
+        return "index() 메서드 호출됨!";
     }
 
     @GetMapping("/hello")
