7e7eba24a5ac1983d68db0dee1554b21802418a0
gandaraipower <jjangjongsoo@naver.com>
2025-08-27 15:18:18 +0900

    13강

--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -2,9 +2,14 @@ package com.mysite.sbb;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
     Optional<Question> findBySubject(String keyword);
+
+    Optional<Question> findBySubjectAndContent(String subject1, String subject2);
+
+    List<Question> findBySubjectLike(String subject);
 }
