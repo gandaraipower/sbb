c5926ac9570e15192c0e59678fdf7ac37ef39d2c
gandaraipower <jjangjongsoo@naver.com>
2025-08-27 11:44:28 +0900

    7강

---
+++ src/main/java/com/mysite/sbb/Answer.java
@@ -0,0 +1,24 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne //N 대 1 answer 객체 기준
+    private Question question;
+}
