ed08d56513c5f6116408e1e7a77de97e057b9778
gandaraipower <jjangjongsoo@naver.com>
2025-08-28 11:48:15 +0900

    23강

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -3,7 +3,6 @@ package com.mysite.sbb.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.stream.Collectors;
 
@@ -13,26 +12,15 @@ public class QuestionController {
 
     private final QuestionRepository questionRepository;
 
-
     @GetMapping("/question/list")
-    @ResponseBody
-    public String list(){
+    public String list() {
 
-        String questions=questionRepository.findAll()
+        String questions = questionRepository.findAll()
                 .stream()
-                .map(e->"<li>%s</li>".formatted(e.getSubject()))
+                .map(e -> "<li>%s</li>".formatted(e.getSubject()))
                 .collect(Collectors.joining("\n"));
 
 
-        return """
-                <html>
-                    <body>
-                        <ul>
-                            %s
-                        </ul>
-                    </body>
-                </html>
-                """.formatted(questions);
+        return "question_list";
     }
-
 }
